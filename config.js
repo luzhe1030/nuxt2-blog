131451f84f77f59375aaf4ba8db3cec834052ae1
luzhe1030 <54615620+luzhe1030@users.noreply.github.com>
2022-11-21 11:06:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: 'scleom', // 网站标题
+  nickName: "scleom", // 昵称
+  githubName: 'luzhe1030', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
-  domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  domain: 'https://blog.scleom.cn', // rss域名
+  SEO_description: '七方雾的blog',
+  SEO_keywords: 'scleom,scleom\'s blog,七方雾的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
